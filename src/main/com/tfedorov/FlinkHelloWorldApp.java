9b49686badac69e38750d1be5bffa42aa1e57900
Taras Fedorov <tfedorov@apple.com>
2024-09-20 12:17:46 +0300

    Init simple HelloWorld APP

---
+++ src/main/com/tfedorov/FlinkHelloWorldApp.java
@@ -0,0 +1,10 @@
+package com.tfedorov;
+
+import java.util.Arrays;
+
+public class FlinkHelloWorldApp {
+
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.toString(args));
+    }
+}
