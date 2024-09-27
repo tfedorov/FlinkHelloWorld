b00d28ae17fe51ca603da96a7ac4c31469130687
Taras Fedorov <tfedorov@apple.com>
2024-09-27 12:40:03 +0300

    Fixed specs

--- src/main/java/com/tfedorov/FlinkConnectionApp.java
+++ src/main/java/com/tfedorov/FlinkConnectionApp.java
@@ -4,10 +4,9 @@ import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.RestOptions;
-import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
-import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
 
 public class FlinkConnectionApp {
@@ -18,16 +17,20 @@ public class FlinkConnectionApp {
         config.setInteger(RestOptions.PORT, 8081);
         StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment(2, config);
 
-        DataStream<Tuple2<String, Integer>> dataStream = env
-                .socketTextStream("localhost", 9999)
-                .flatMap(new Splitter())
-                .keyBy(value -> value.f0)
-                .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
-                .sum(1);
+        DataStreamSource<String> inputDataStream = env.socketTextStream("localhost", 9999);
 
-        dataStream.print();
+        SingleOutputStreamOperator<Tuple2<String, Integer>> resultStream = sumStream(inputDataStream);
+
+        resultStream.print();
 
         env.execute("Window WordCount");
+    }
+
+    static SingleOutputStreamOperator<Tuple2<String, Integer>> sumStream(DataStreamSource<String> inputDataStream) {
+        return inputDataStream.flatMap(new Splitter())
+                .keyBy(value -> value.f0)
+//                .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+                .sum(1);
     }
 
     public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
--- src/test/java/com/tfedorov/FlinkConnectionAppTest.java
+++ src/test/java/com/tfedorov/FlinkConnectionAppTest.java
@@ -2,49 +2,85 @@ package com.tfedorov;
 
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration;
-import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
-import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.test.util.MiniClusterWithClientResource;
+import org.apache.flink.util.Collector;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FlinkConnectionAppTest {
 
-
     @Test
-    public void testWindowWordCount() throws Exception {
+    public void testSumStream() throws Exception {
         MiniClusterWithClientResource flinkCluster = new MiniClusterWithClientResource(
                 new MiniClusterResourceConfiguration.Builder()
-                        .setNumberTaskManagers(1)
+                        .setNumberTaskManagers(2)
                         .setNumberSlotsPerTaskManager(2)
                         .build());
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         // Mock input data
-        List<String> input = Arrays.asList("hello world", "hello flink", "flink testing");
+        List<String> input = asList("hello world", "hello flink", "flink testing");
 
-        DataStream<Tuple2<String, Integer>> dataStream = env
-                .fromCollection(input)
-                .flatMap(new FlinkConnectionApp.Splitter())
-                .keyBy(value -> value.f0)
-                .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
-                .sum(1);
+        DataStreamSource<String> inputStream = env.fromCollection(input);
 
-        // Collect results into a list
-        List<Tuple2<String, Integer>> result = dataStream.executeAndCollect(3);
+        SingleOutputStreamOperator<Tuple2<String, Integer>> actualStream = FlinkConnectionApp.sumStream(inputStream);
+        actualStream.print();
+        actualStream.getTransformation();
+        List<Tuple2<String, Integer>> actual = actualStream
+                .executeAndCollect(100).stream().sorted(Comparator.comparing(tuple -> tuple.f0))
+                .collect(Collectors.toList());
 
-        // Validate the result
-        assertEquals(Arrays.asList(
-                new Tuple2<>("hello", 2),
-                new Tuple2<>("world", 1),
-                new Tuple2<>("flink", 2),
-                new Tuple2<>("testing", 1)
-        ), result);
+        List<Tuple2<String, Integer>> expected = Stream.of(
+                        new Tuple2<>("testing", 1),
+                        new Tuple2<>("hello", 1),
+                        new Tuple2<>("hello", 2),
+                        new Tuple2<>("world", 1),
+                        new Tuple2<>("flink", 1),
+                        new Tuple2<>("flink", 2)
+                ).sorted(Comparator.comparing(tuple -> tuple.f0))
+                .collect(Collectors.toList());
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFlatMap() throws Exception {
+        // Arrange
+        FlinkConnectionApp.Splitter splitter = new FlinkConnectionApp.Splitter();
+        // Create a mock collector to collect the output
+        List<Tuple2<String, Integer>> collectedItems = new ArrayList<>();
+
+        Collector<Tuple2<String, Integer>> collector = new Collector<Tuple2<String, Integer>>() {
+            @Override
+            public void collect(Tuple2<String, Integer> element) {
+                collectedItems.add(element);
+            }
+
+            @Override
+            public void close() {
+                // No operation
+            }
+        };
+
+        // Act
+        String inputSentence = "hello world hello";
+        splitter.flatMap(inputSentence, collector);
+
+        // Assert
+        List<Tuple2<String, Integer>> expectedOutput = asList(new Tuple2<>("hello", 1), new Tuple2<>("world", 1), new Tuple2<>("hello", 1));
+
+        assertEquals(expectedOutput, collectedItems);
+
     }
 }
